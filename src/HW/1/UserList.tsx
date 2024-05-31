12faf175a9df3a7fe4895f79ff2b6109badde9cb
Evgeniya13 <zxzxzx1307@gmail.com>
2024-05-31 14:06:16 -0600

    HW2

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,29 +1,29 @@
+import React from 'react';
+
 type AddressType = {
   street: string;
   city: string;
 };
 
 type UserType = {
-  id:   number;
+  id: number;
   name: string;
-  age:  number;
+  age: number;
   address: AddressType;
 };
-
 
 type UserListPropsType = {
   users: UserType[];
 };
 
-
 export const UserList = (props: UserListPropsType) => {
   return (
-    <div id={'hw01-users'}>
+    <div id="hw01-users">
       <h2>User List:</h2>
       <ul>
         {props.users.map((user) => (
           <li key={user.id} id={`hw01-user-${user.id}`}>
-            <strong>{user.name}</strong> (Age: {user.age}) <strong>Address:</strong> 
+            <strong>{user.name}</strong> (Age: {user.age}) <strong>Address:</strong>
             {user.address.street}, {user.address.city}
           </li>
         ))}
